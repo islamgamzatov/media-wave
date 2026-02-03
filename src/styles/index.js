1e54b91b69829b25706e6852051a6d4a01c7fb0d
Islam <gamzatovislam314@gmail.com>
2026-02-03 12:41:37 +0300

    feat: add base project styles and fonts
    ## Prepared common style files:
    • Added fonts and connected in fonts.scss via font-face directive
    • Defined project variables in variables.scss (colors, spacing, typography)
    • Utility classes in utils.scss (container, visibility helpers)
    • Global styles in globals.scss
    • All imported in index.js

---
+++ src/styles/index.js
@@ -0,0 +1,5 @@
+import '@a1rth/css-normalize/index.css' // Подключение файла нормализации стилей.
+import './fonts.scss' // Подключение файла шрифтов.
+import './variables.scss' // Подключение файла с переменными.
+import './utils.scss' // Подключение файла с утилитарными классами.
+import './globals.scss' // Подключаем файл с глобальными стилями.
